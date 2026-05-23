2ab41bc7c6f0c6a860a4c4991bbc2919d4b27323
Khusrav-afk <155685012+Khusrav-afk@users.noreply.github.com>
2026-05-23 16:11:24 +0500

    Update page.js

--- app/training/page.js
+++ app/training/page.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-
+export const dynamic = 'force-dynamic'
 export const metadata = { title: 'Обучение — Beauty Shape' }
 
 export default function TrainingPage() {
