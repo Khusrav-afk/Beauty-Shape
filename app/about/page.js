1afb99fdb3c4a4ebdabfefcf14f12a30ae00c46b
Khusrav-afk <155685012+Khusrav-afk@users.noreply.github.com>
2026-05-23 16:09:41 +0500

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,7 @@
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import Link from 'next/link'
-
+export const dynamic = 'force-dynamic'
 export const metadata = { title: 'О компании — Beauty Shape' }
 
 export default function AboutPage() {
