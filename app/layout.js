c50f0c227b2f8632003fdbdd2cacaa2ff4a17d6b
Khusrav-afk <155685012+Khusrav-afk@users.noreply.github.com>
2026-06-13 10:01:13 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import BottomNav from '@/components/BottomNav'
 import { CartProvider } from '@/components/CartContext'
+import { AuthProvider } from '@/components/AuthContext'
 
 export const dynamic = 'force-dynamic'
 
@@ -13,10 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ru">
       <body>
-        <CartProvider>
-          {children}
-          <BottomNav />
-        </CartProvider>
+        <AuthProvider>
+          <CartProvider>
+            {children}
+            <BottomNav />
+          </CartProvider>
+        </AuthProvider>
       </body>
     </html>
   )
