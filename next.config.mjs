602732509f3546a75d1fddb86421c3a649664673
Khusrav-afk <155685012+Khusrav-afk@users.noreply.github.com>
2026-07-06 14:34:39 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        // Любой заход на технический адрес *.vercel.app → на основной домен
+        source: '/:path*',
+        has: [{ type: 'host', value: 'beauty-shape.vercel.app' }],
+        destination: 'https://beautyi-shape.ru/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 export default nextConfig
